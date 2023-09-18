a7c89bf2bbb7efeb3b5feee788e8d419a9b89d44
Marcel Obiefuna <obiefunamarcel@gmail.com>
2023-09-18 02:32:31 +0100

    Setting up creating session and login ===================================== 1. Create a new file called libs/auth.js 2. Create a new file called models.js 3. Create a new file called views/dashboard.ejs  I setup the dashboard.ejs to show the user's email address and a logout button 4. Modify the views/auth/login.ejs to show the login error message

--- passport.js
+++ passport.js
@@ -1,3 +1,26 @@
-const localStrategy = require("passport-local").Strategy
-const initialize=(passport)=>{}
-module.exports=initialize
+const LocalStrategy = require("passport-local").Strategy
+const bcrypt = require("bcrypt"); // this is a pkg used for end to end password encrypton
+
+const initialize = (passport, getUserByEmail, getUserById) => {
+    const authenticateUser = async (email, password, done) => {
+        const user = await getUserByEmail(email);
+        if (user == null) {
+            return done(null, false, { message: "No user with that email" })
+        }
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: "Password incorrect" })
+            }
+        } catch (e) {
+            return done(e)
+        }
+    }
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser)); 
+    passport.serializeUser((user, done) => done(null, user._id));
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    });
+}
+module.exports = initialize
