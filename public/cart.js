51c2e8bd54b63b9d5b9496fc4029fe9604870754
Jude <jude.eze1700@gmail.com>
2023-10-18 11:17:13 +0300

    updated

--- public/cart.js
+++ public/cart.js
@@ -1,34 +1,38 @@
-let cart = document.querySelector(".cart-list")
-const addItem=(event) =>{
-    let clickButton=event.target
-    let parent=clickButton.parentElement
-    let productDetail=parent.parentElement
-   let productName=productDetail.querySelector(".name").innerText
-   let productPrice=productDetail.querySelector(".price").innerText
-   //let productImage=productDetail.querySelector(".product-image>a>img")
+// let cart = document.querySelector(".cart-list")
+// const addItem=(event) =>{
+//     let clickButton=event.target
+//     let parent=clickButton.parentElement
+//     let productDetail=parent.parentElement
+//    let productName=productDetail.querySelector(".name").innerText
+//    let productPrice=productDetail.querySelector(".price").innerText
+//    //let productImage=productDetail.querySelector(".product-image>a>img")
    
-   let newItem = document.createElement("li") 
-   newItem.classList.add("product-box-contain")
-   newItem.innerHTML=`<div class="drop-cart">
-   <a href="product-left-thumbnail.html" class="drop-image">
-   <img src="../assets/images/vegetable/product/2.png" class="blur-up lazyload" alt="">
-</a>
-   <div class="drop-contain">
-       <a href="product-left-thumbnail.html">
-           <h5>${productName}</h5>
-       </a>
-       <h6><span>1 x</span> ${productPrice}</h6>
-       <button class="close-button close_button">
-           <i class="fa-solid fa-xmark"></i>
-       </button>
-   </div>
-</div>`
-   cart.appendChild(newItem)
-}
-let addButtons= document.querySelectorAll(".judecart")
-addButtons.forEach((addButton)=>{
-    addButton.addEventListener("click",addItem)
-})
-let updatePrice= ()=>{
+//    let newItem = document.createElement("li") 
+//    newItem.classList.add("product-box-contain")
+//    newItem.innerHTML=`<div class="drop-cart">
+//    <a href="product-left-thumbnail.html" class="drop-image">
+//    <img src="../assets/images/vegetable/product/2.png" class="blur-up lazyload" alt="">
+// </a>
+//    <div class="drop-contain">
+//        <a href="product-left-thumbnail.html">
+//            <h5>${productName}</h5>
+//        </a>
+//        <h6><span>1 x</span> ${productPrice}</h6>
+//        <button class="close-button close_button">
+//            <i class="fa-solid fa-xmark"></i>
+//        </button>
+//    </div>
+// </div>`                                                                                                                                                                                                                                                                                 
+//    cart.appendChild(newItem)
+// }
+// let addButtons= document.querySelectorAll(".judecart")
+// addButtons.forEach((addButton)=>{
+//     addButton.addEventListener("click",addItem)
+// })
+// let updatePrice= ()=>{
 
-}
+// }
+let priceBox=document.querySelector(".price-box>h4").innerText
+let total=priceBox.replace("$"," ") 
+
+console.log(priceBox);
